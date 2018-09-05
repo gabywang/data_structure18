d3d9ad3f3a9463a3cfa9f511b4c6443c73f8758a
Gaby <gabywangxiaowen@berkeley.edu>
2018-09-06 04:28:58 +0800

    print the universe.

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -64,6 +64,14 @@ public class NBody {
             StdDraw.pause(10);
             drawPlanets(planets);
             t += dt;
+        }
+        /**Print the universe. */
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < planets.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                          planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+                          planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
         }       
     }
 }
