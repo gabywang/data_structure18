d1e890425aecc3a05053e075ab07e77074e44431
Gaby <gabywangxiaowen@berkeley.edu>
2018-10-13 22:24:21 -0400

    complete remove function and get function. Need to fix resize function.

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -27,9 +27,9 @@ public class ArrayDeque<T> {
     private int changeIndex(int index) {
         if(index < 0) {
             return items.length + index;
-        }else if(index >= items.length) {
+        } else if(index >= items.length) {
             return index - items.length;
-        }else {
+        } else {
             return index;
         }
     }
@@ -40,7 +40,7 @@ public class ArrayDeque<T> {
             resize(size*2);
         }
         items[nextFirst] = x;
-        size +=1;
+        size ++;
         nextFirst = changeIndex(nextFirst - 1);
     }
 
@@ -50,7 +50,54 @@ public class ArrayDeque<T> {
             resize(size * 2);
         }
         items[nextLast] = x;
-        size +=1;
+        size ++;
         nextLast = changeIndex(nextLast + 1);
     }
+
+    /**  Returns the number of items in the deque.*/
+    public int size() {
+        return size;
+    }
+
+    /** Removes and returns the item at the front of the deque.
+     *  If no such item exists, returns null.*/
+    public T removeFirst() {
+        if(size == 0) {
+            return null;
+        }
+        if(items.length >= 16 && size < items.length / 4) {
+            resize(items.length/2);
+        }
+        size --;
+        T afterRemove = items[changeIndex(nextFirst + 1)];
+        items[changeIndex(nextFirst+1)] = null;
+        nextFirst =changeIndex(nextFirst+1);
+        return afterRemove;
+    }
+
+    /** Removes and returns the item at the back of the deque.
+     *  If no such item exists, returns null.*/
+    public T removeLast() {
+        if(size == 0) {
+            return null;
+        }
+        if(items.length >= 16 && size < items.length / 4) {
+            resize(items.length/2);
+        }
+        size --;
+        T afterRemove = items[changeIndex(nextLast - 1)];
+        items[changeIndex(nextLast - 1)] = null;
+        nextLast =changeIndex(nextLast - 1);
+        return afterRemove;
+    }
+
+    /** Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+     *  If no such item exists, returns null. Must not alter the deque!*/
+    public T get(int index) {
+        if(index > size-1) {
+            return null;
+        }
+        return items[changeIndex(nextFirst + 1 + index)];
+    }
 }
+
